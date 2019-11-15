1a9331ee250c385f1db8ec563b6b4806ec3ed1a7
Matthias <MatthiasSaihttam1@gmail.com>
2019-11-15 14:47:43 -0500

    Updates for Vivaldi 2.9
    No guarantees, but this should work a little better in 2019

--- custom.js
+++ custom.js
@@ -1,14 +1,12 @@
 'use strict';
-requestIdleCallback(() => {
+function applyModifications () {
     const toolbar = document.querySelector('#main>.toolbar');
     const webview = document.querySelector('#webview-container');
     const tabs = document.querySelector('#tabs-container .tab-strip');
     const tabsSwitch = document.querySelector('#switch>button.tabs');
-
-    if(toolbar && webview && tabs && tabsSwitch){
-        toolbar.classList.add('vt-shown');
-
-        const input = toolbar.querySelector('.addressfield input');
+    
+    //Create a function to add the event listerners to the address bar input
+    function addEventListenersToInput(input) {
         input.addEventListener('focus', () => {
             clearTimeout(toolbar.vt_hide_timeout);
             delete toolbar.vt_hide_timeout;
@@ -22,6 +20,13 @@ requestIdleCallback(() => {
                 }, 200);
             }
         });
+    }
+    
+    if(toolbar && webview && tabs && tabsSwitch){
+        toolbar.classList.add('vt-shown');
+        
+        let input = toolbar.querySelector('.addressfield input');
+        
         toolbar.addEventListener('mousedown', () => {
             clearTimeout(toolbar.vt_hide_timeout);
             toolbar.vt_hide_timeout = setTimeout(() => {
@@ -35,12 +40,13 @@ requestIdleCallback(() => {
             input.focus();
         });
         webview.appendChild(toggle);
-
-        const reload = toolbar.querySelector('.button-toolbar.reload');
+        
+        //Reload button no longer has the .reload class, so we have to find it and check loading state by its title (Breaks in non-English)
+        const reload = toolbar.querySelector("button[title=\"Reload current page\"");
         const reloadObserver = new MutationObserver(mutationsList => {
             for(var mutation of mutationsList) {
                 if (mutation.type === 'attributes') {
-                    if(reload.classList.contains('loading')){
+                    if(reload.title === "Stop"){
                         toolbar.classList.add('vt-shown2');
                     }
                     else{
@@ -50,7 +56,23 @@ requestIdleCallback(() => {
             }
         });
         reloadObserver.observe(reload, { attributes: true });
-
+        
+        //Creates a new MutationObserver to watch the element above the address bar. The addressbar element gets replaced when the page reloads, so this watches for that and re-adds the correct event listeners and classes
+        const inputWrapper = toolbar.querySelector(".addressfield .observer");
+        const inputReplacedObserver = new MutationObserver(mutationsList => {
+            for(var mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    input = toolbar.querySelector('.addressfield input');
+                    addEventListenersToInput(input);
+                    if (document.activeElement === input) {
+                        input.classList.add("vt-shown");
+                    }
+                }
+            }
+        });
+        inputReplacedObserver.observe(inputWrapper, { childList: true });
+        
+        
         const hookBookmark = () => {
             const bookmark = toolbar.querySelector('.add-bookmark-container');
             if(bookmark){
@@ -109,12 +131,13 @@ requestIdleCallback(() => {
                         node.appendChild(thumbnail);
 
                         const getThumb = () => {
-                            const thumblink = document.querySelector('#tabs-container #tab-' + tab.id + '>.thumbnail-image');
+                            const thumblink = document.querySelector('#tabs-container #tab-' + tab.id + '>.thumbnail-image>img');
                             if(thumblink){
                                 node.thumblink = thumblink;
-                                thumbnail.style.backgroundImage = thumblink.style.backgroundImage;
+                                //The tab image is now stored in the `src` of an `img` element
+                                thumbnail.style.backgroundImage = `url("${thumblink.src}")`;
                                 node.observer = new MutationObserver(() => {
-                                    thumbnail.style.backgroundImage = thumblink.style.backgroundImage;
+                                    thumbnail.style.backgroundImage = `url("${thumblink.src}")`;
                                 });
                                 node.observer.observe(thumblink, {attributes: true});
                                 return true;
@@ -123,16 +146,14 @@ requestIdleCallback(() => {
                                 return false;
                             }
                         };
-                        if(!getThumb()){
-                            setTimeout(() => {
-                                if(!getThumb()){
-                                    setTimeout(() => {
-                                        setTimeout(getThumb, 500);
-                                    }, 200);
-                                }
-                            }, 100);
+                        //Trys to set the thumbnail incessantly. It seems to take much longer to update 
+                        function trySetThumb () {
+                            console.log("Trying to set thumbnail for tab #" + tab.id);
+                            if (!getThumb()) {
+                                setTimeout(trySetThumb, 300);
+                            }
                         }
-
+                        trySetThumb();
 
                         const close = document.createElement('div');
                         close.className = 'close';
@@ -210,4 +231,13 @@ requestIdleCallback(() => {
         });
         switchObserver.observe(tabsSwitch, { attributes: true });
     }
+}
+
+window.addEventListener("load", function () {
+    const interval = window.setInterval(() => {
+        if (document.querySelector('#main>.toolbar')) {
+            window.clearInterval(interval);
+            applyModifications();
+        }
+    }, 300);
 });
